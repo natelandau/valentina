b310f4a7fd45453f8a052e4eb65ce60d9aabd890
Nathaniel Landau <nate@natelandau.com>
2023-06-02 18:06:19 -0400

    feat(debug): add `reload_cogs` to `debug` cog
    allow live reloading of cogs without requiring the bot to be bounced

--- src/valentina/cogs/debug.py
+++ src/valentina/cogs/debug.py
@@ -1,4 +1,6 @@
 """Sample cog used for testing purposes."""
+from pathlib import Path
+
 import discord
 from discord.ext import commands
 from loguru import logger
@@ -15,12 +17,30 @@ class Debug(commands.Cog):
     debug = discord.SlashCommandGroup("debug", "Debug related commands")
 
     @debug.command(description="Receive debug information about the bot.")
+    @commands.is_owner()
     async def ping(self, ctx: discord.ApplicationContext) -> None:
         """Ping the bot to get debug information."""
         logger.info("debug:ping: Generating debug information")
         await ctx.respond(
             f"Status is {self.bot.status}\nLatency is {self.bot.latency}\nConnected to {len(self.bot.guilds)} guilds"
         )
+
+    @debug.command(description="Live reload all cogs.")
+    @commands.is_owner()
+    async def reload_cogs(self, ctx: discord.ApplicationContext) -> None:
+        """Reloads all cogs."""
+        logger.debug("debug:reload: Reloading all cogs...")
+        count = 0
+        for cog in Path(self.bot.parent_dir / "src" / "valentina" / "cogs").glob("*.py"):
+            if cog.stem[0] != "_":
+                count += 1
+                logger.debug(f"COGS: Loading - {cog.stem}")
+                self.bot.reload_extension(f"valentina.cogs.{cog.stem}")
+
+        embed = discord.Embed(
+            title="Reload Cogs", description=f"{count} cogs successfully reloaded", color=0xFF00C8
+        )
+        await ctx.respond(embed=embed)
 
 
 def setup(bot: Valentina) -> None:
