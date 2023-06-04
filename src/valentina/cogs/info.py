4a3884127b8d28db157c37186d2e25ef74dae423
Nathaniel Landau <nate@natelandau.com>
2023-06-04 11:57:52 -0400

    feat(info): add `health` command to show health levels

---
+++ src/valentina/cogs/info.py
@@ -0,0 +1,42 @@
+"""Game information cog for Valentina."""
+
+import discord
+from discord.ext import commands
+from loguru import logger
+
+from valentina.models import Valentina
+
+
+class Info(commands.Cog):
+    """Commands for help."""
+
+    def __init__(self, bot: Valentina) -> None:
+        self.bot = bot
+
+    info = discord.SlashCommandGroup("info", "Get information about the game")
+
+    @info.command(description="See health levels.")
+    async def health(self, ctx: discord.ApplicationContext) -> None:
+        """Display health levels."""
+        description = "```\n"
+        description += "Bruised       :\n"
+        description += "Hurt          : -1\n"
+        description += "Injured       : -1\n"
+        description += "Wounded       : -2\n"
+        description += "Mauled        : -2\n"
+        description += "Crippled      : -5\n"
+        description += "Incapacitated :\n"
+        description += "```"
+
+        embed = discord.Embed(
+            title="Health Levels",
+            description=description,
+            color=discord.Color.red(),
+        )
+        logger.debug(f"INFO: {ctx.author.display_name} requested health levels")
+        await ctx.send(embed=embed)
+
+
+def setup(bot: Valentina) -> None:
+    """Add the cog to the bot."""
+    bot.add_cog(Info(bot))
