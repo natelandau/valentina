2dfb4cec8ec9721d8b989254afb8aa123c8ffd56
Nathaniel Landau <nate@natelandau.com>
2024-12-22 18:38:22 -0500

    test: more tests

--- tests/models/test_dictionaryterm.py
+++ tests/models/test_dictionaryterm.py
@@ -9,11 +9,14 @@ from valentina.models import DictionaryTerm
 @pytest.mark.drop_db
 async def test_event_based_actions():
     """Test the DictionaryTerm model."""
+    # Given: A dictionary term with whitespace and mixed case
     test_name = " TEST TERM "
     test_synonyms = ["TEST SYNONYM 1", "TEST SYNONYM 2 ", " "]
 
+    # When: Creating and inserting a new dictionary term
     new_term = DictionaryTerm(term=test_name, synonyms=test_synonyms, guild_id=1234567890)
     await new_term.insert()
 
+    # Then: The term and synonyms are normalized to lowercase with whitespace trimmed
     assert new_term.term == "test term"
     assert new_term.synonyms == ["test synonym 1", "test synonym 2"]
