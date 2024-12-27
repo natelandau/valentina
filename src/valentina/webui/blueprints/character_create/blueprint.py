38b32802af17f0704eb7189a73372a8066cc4395
Nathaniel Landau <natelandau@users.noreply.github.com>
2024-12-27 13:36:07 -0500

    fix(webui): fix broken character creation (#224)

--- src/valentina/webui/blueprints/character_create/blueprint.py
+++ src/valentina/webui/blueprints/character_create/blueprint.py
@@ -2,11 +2,7 @@
 
 from quart import Blueprint
 
-from .route_create_full import (
-    CreateCharacterStep1,
-    CreateCharacterStep2,
-    CreateCharacterStep3,
-)
+from .route_create_full import CreateFull1, CreateFull2
 from .route_rng_player import CreateRNGCharacter
 from .route_rng_storyteller import CreateStorytellerRNGCharacter
 from .route_start import StartCharacterCreate
@@ -18,21 +14,19 @@ blueprint.add_url_rule(
     "/create_character", view_func=StartCharacterCreate.as_view("start"), methods=["GET", "POST"]
 )
 
+### Create Full Characters
 blueprint.add_url_rule(
-    "/create_character/1",
-    view_func=CreateCharacterStep1.as_view("create_1"),
+    "/create_character/create_1",
+    view_func=CreateFull1.as_view("create_1"),
     methods=["GET", "POST"],
 )
 blueprint.add_url_rule(
     "/create_character/2/<string:character_id>/<string:char_class>",
-    view_func=CreateCharacterStep2.as_view("create_2"),
+    view_func=CreateFull2.as_view("create_2"),
     methods=["GET", "POST"],
 )
-blueprint.add_url_rule(
-    "/create_character/3/<string:character_id>",
-    view_func=CreateCharacterStep3.as_view("create_3"),
-    methods=["GET", "POST"],
-)
+
+### Create RNG Characters
 blueprint.add_url_rule(
     "/create_character/rng/player",
     view_func=CreateRNGCharacter.as_view("rng_player"),
